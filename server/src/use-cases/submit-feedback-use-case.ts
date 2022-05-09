89bfd5bd5187896567192c4e4ced7fe3135fa2e9
Manoel Rocha <manoelmelo367@gmail.com>
2022-05-09 20:01:11 +0000

    email estilizado

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -1,10 +1,10 @@
-import { MailAdapter } from "../adapters/mail-adapter";
-import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+import { MailAdapter } from '../adapters/mail-adapter'
+import { FeedbacksRepository } from '../repositories/feedbacks-repository'
 
 interface SubmitFeedbackUseCaseRequest {
-  type: string;
-  comment: string;
-  screenshot?: string;
+  type: string
+  comment: string
+  screenshot?: string
 }
 
 export class SubmitFeedbackUseCase {
@@ -14,34 +14,62 @@ export class SubmitFeedbackUseCase {
   ) {}
 
   async execute(request: SubmitFeedbackUseCaseRequest) {
-    const {type, comment, screenshot} = request;
+    const { type, comment, screenshot } = request
 
-    if(!type) {
-      throw new Error('Type and comment are required');
+    if (!type) {
+      throw new Error('Type and comment are required')
     }
 
-    if(!comment) {
-      throw new Error('comment are required');
+    if (!comment) {
+      throw new Error('comment are required')
     }
 
-    if(screenshot && !screenshot.startsWith('data:image/png;base64')) {
-      throw new Error('Invalid screenshot');
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot')
     }
 
     await this.feedbacksRepository.create({
       type,
       comment,
       screenshot
-    });
+    })
 
     await this.mailAdapter.sendMail({
-      subject: "Novo Feedback",
+      subject: 'Novo Feedback',
       body: [
-        `<div style="font-family: sans-serif; font-size: 16px; color: #111; align-items: "center"" >`,
+        `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />`,
+        `<style type="text/css">
+        body {
+          font-family: sans-serif;
+          font-size: 16px;
+          color: #111;
+        }
+        div {
+          align-items: center;
+        }
+  
+        img {
+          width: 100%;
+          height: auto;
+        }`,
+        `</style>`,
+        `<title>Email</title>
+        </head>
+        <body>`,
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111; align-items: center" >`,
         `<p>Tipo de feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
-        screenshot ? `<img src="${screenshot}" style="height: 300px; width: 500px" />` : '',
-        `</div>`
+        screenshot
+          ? `<img src="${screenshot}" style="height: 700px; width: 1000px" />`
+          : '',
+        `</div>`,
+        ` </body>
+        </html>`
       ].join('\n')
     })
   }
