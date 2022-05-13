97b52745d15cf3a18cd9daef7e2d3ef0e409f24b
Manoel Rocha <manoelmelo367@gmail.com>
2022-05-13 20:29:55 +0000

    aalterado smtp

--- server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -3,25 +3,25 @@ import nodemailer from 'nodemailer'
 
 
 const transport = nodemailer.createTransport({
-  host: "smtp-relay.sendinblue.com",
-  port: 587,
+  host: "smtp.elasticemail.com",
+  port: 2525,
   auth: {
-    user: "manoelmelo367@gmail.com",
-    pass: "8WkcYLCVHz3fgpKD"
+    user: process.env.MAIL_USER,
+    pass: process.env.PASSWORD
   }
 })
 
 export class NodemailerMailAdapter implements MailAdapter {
 async sendMail({subject, body, screenshot}: MailAdapterData) {
  await transport.sendMail({
-    from: 'Feedback Feedget <oi@feedget.com>',
-    to: 'Manoel Rocha <manoelmelo367@gmail.com>',
+    from: 'Feedback Feedget <manoelmelo367@gmail.com>',
+    to: 'Manoel Rocha <manoelsec01@gmail.com>',
     subject,
     html: body,
     attachments: [{
       filename: 'screenshot.png',
       cid: 'screenshot',
-      path: screenshot,
+      path: screenshot || '',
       content: (screenshot || ''),
       contentType: 'image/png'
     }]
